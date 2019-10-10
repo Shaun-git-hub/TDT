7422772504c37c9ea4ae6bfa2ea8505c7bd4ae0a
Shaun <shaunn@aliyun.com>
2019-10-10 18:38:51 +0800

    time

---
+++ chiron/netWork/net.c
@@ -0,0 +1,18 @@
+/* service */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main()
+{
+    int ret;
+
+    ret = socket(int domain, SOCK_STREAM, AF_INET);
+
+    
+}
+
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 Git is a distributed version control system
 GIT IS FREE SOFTWARE
+/* 阅读代码是非诚重要的事情，特别阅读一些优秀的代码，对于你的编程
+    是有极大的好处 */
